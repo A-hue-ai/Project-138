0275022da9a59edf00295e3832f7b80e88b05c5e
A-hue-ai <78849972+A-hue-ai@users.noreply.github.com>
2021-04-21 17:52:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup()
+{
+    canvas = createCanvas(650, 400);
+    video = createCapture(VIDEO);
+    video.size(600,300);
+
+    poseNet = ml5.posenet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
