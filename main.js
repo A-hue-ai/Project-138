3c331050f12252e67732cc60ecb0409e4d550f2c
A-hue-ai <78849972+A-hue-ai@users.noreply.github.com>
2021-04-25 16:15:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-function setup()
-{
-    canvas = createCanvas(650, 400);
-    video = createCapture(VIDEO);
-    video.size(600,300);
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
 
-    poseNet = ml5.posenet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
+game_status = "";
 
-function modelLoaded()
-{
-    console.log('Model Loaded!');
-}
-
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+  }
+  
+  function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+  
+      rightWristY = results[0].pose.rightWrist.y;
+      rightWristX = results[0].pose.rightWrist.x;
+      scoreRightWrist =  results[0].pose.keypoints[10].score;
+      console.log(scoreRightWrist);
+    }
+  }
+  
+  function startGame()
+  {
+    game_status = "start";
+    document.getElementById("status").innerHTML = "Game Is Loading";
+  }
+  
